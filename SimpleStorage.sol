41581a495a3886bf57ce2f71907f3bdd45ba614f
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-07 23:59:04 -0500

    Create & update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.14;
 
 contract SimpleStorage {
-    bool myBoolean = true;
-    uint256 myUnsignedInteger = 123;
-    int256 myInteger = -123;
-    string myString = "String";
-    address myAddress = 0xa7a0275220A00ae3B360F7cB080069063e886271;
-    bytes32 myBytes = "Bytes";
+    bool public myBoolean = true;
+    uint256 public myUnsignedInteger = 123;
+    int256 public myInteger = -123;
+    string public myString = "String";
+    address public myAddress = 0xa7a0275220A00ae3B360F7cB080069063e886271;
+    bytes32 public myBytes = "Bytes";
 
     function store(uint256 _myUnsignedInteger) public {
         myUnsignedInteger = _myUnsignedInteger;
